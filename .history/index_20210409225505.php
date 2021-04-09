7adf6e36a3428c8b1583927b9d3f7ebca1a47003
Saint-S-Prit <mamadoulaminediop93@gmail.com>
2021-04-09 22:56:37 +0000

    yesy

---
+++ .history/index_20210409225505.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel repellendus quia, doloribus aliquid atque commodi odio! Ipsum cumque animi nesciunt facere nemo, magnam natus nobis eos quas, illum nisi eveniet?
+</body>
+
+</html>
